d1e805db7f5570b560f864cdc7fad0c098cf41c5
lukasyato <isoaeyato@gmail.com>
2021-03-27 11:48:02 +0700

    yourchara sourcecode v1.0
    first version yourchara sourcecode

---
+++ sourcecode/yourchara/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace yourchara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.track_high = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.track_low = new System.Windows.Forms.TrackBar();
+            this.volumelabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.track_high)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_low)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.track_high);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.track_low);
+            this.groupBox1.Controls.Add(this.volumelabel);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(466, 127);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(394, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "v1.0\r\nby lukasyato";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(263, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "High Talk";
+            // 
+            // track_high
+            // 
+            this.track_high.Location = new System.Drawing.Point(6, 94);
+            this.track_high.Maximum = 100;
+            this.track_high.Name = "track_high";
+            this.track_high.Size = new System.Drawing.Size(251, 45);
+            this.track_high.TabIndex = 5;
+            this.track_high.Value = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(263, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Low Talk";
+            // 
+            // track_low
+            // 
+            this.track_low.Location = new System.Drawing.Point(6, 67);
+            this.track_low.Maximum = 100;
+            this.track_low.Name = "track_low";
+            this.track_low.Size = new System.Drawing.Size(251, 45);
+            this.track_low.TabIndex = 3;
+            this.track_low.Value = 4;
+            // 
+            // volumelabel
+            // 
+            this.volumelabel.AutoSize = true;
+            this.volumelabel.BackColor = System.Drawing.Color.Transparent;
+            this.volumelabel.Location = new System.Drawing.Point(123, 47);
+            this.volumelabel.Name = "volumelabel";
+            this.volumelabel.Size = new System.Drawing.Size(13, 13);
+            this.volumelabel.TabIndex = 2;
+            this.volumelabel.Text = "0";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(251, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Lime;
+            this.progressBar1.Location = new System.Drawing.Point(6, 46);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(251, 15);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Background Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(447, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(490, 467);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "yourchara";
+            this.AutoSizeChanged += new System.EventHandler(this.Form1_AutoSizeChanged);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.track_high)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_low)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label volumelabel;
+        private System.Windows.Forms.TrackBar track_low;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar track_high;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ sourcecode/yourchara/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace yourchara
+{
+    public partial class Form1 : Form
+    {
+        private Image img_idle = Image.FromFile("chara0.png");
+        private Image img_lowtalk = Image.FromFile("chara1.png");
+        private Image img_hightalk = Image.FromFile("chara2.png");
+        private Image nowimg = Image.FromFile("chara0.png");
+        private int global_delta_r = 100;
+        private float global_delta = 50;
+        private WaveIn recorder;
+        private bool nowrecording = false;
+        private Color bg_color = Color.Lime;
+        private Graphics img_graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            img_graphics = this.CreateGraphics();
+            this.SetStyle(
+            ControlStyles.AllPaintingInWmPaint |
+            ControlStyles.UserPaint |
+            ControlStyles.DoubleBuffer, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            int waveInDevices = WaveIn.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(waveInDevice);
+                string get = String.Format("Device {0}: {1}, {2} channels", waveInDevice, deviceInfo.ProductName, deviceInfo.Channels);
+                comboBox1.Items.Add(get);
+            }
+            this.ReDraw();
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs args)
+        {
+            float max = 0;
+            // interpret as 16 bit audio
+            for (int index = 0; index < args.BytesRecorded; index += 2)
+            {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                                        args.Buffer[index + 0]);
+                // to floating point
+                var sample32 = sample / 32768f;
+                // absolute value 
+                if (sample32 < 0) sample32 = -sample32;
+                // is this the max value?
+                if (sample32 > max) max = sample32;
+            }
+            progressBar1.Value = (int)(max * 100f);
+            volumelabel.Text = progressBar1.Value.ToString();
+            bool somechange = false;
+            if (progressBar1.Value >= track_low.Value && nowimg != img_lowtalk)
+            {
+                nowimg = img_lowtalk;
+                somechange = true;
+            }
+            else if (progressBar1.Value >= track_high.Value && nowimg != img_hightalk)
+            {
+                nowimg = img_hightalk;
+                somechange = true;
+            }
+            else if (nowimg != img_idle)
+            {
+                nowimg = img_idle; 
+                somechange = true;
+            }
+            if (somechange)
+            {
+                global_delta = 50;
+                this.Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.AllowFullOpen = true;
+            colorDlg.AnyColor = true;
+            colorDlg.SolidColorOnly = false;
+            colorDlg.Color = bg_color;
+
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                bg_color = colorDlg.Color;
+                this.BackColor = bg_color;
+                button2.ForeColor = bg_color;
+            }  
+        }
+
+        private int Lerp(int a, int b, float t)
+        {
+            float tp = 1.0f - t;
+            return (int)(tp * a + t * b);
+        }
+
+        private void ReDraw()
+        {
+            this.Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //global_delta = (float) this.Lerp((int)global_delta, global_delta_r, 0.001f);
+            float delta = 100f;
+            float size = this.Height;
+            e.Graphics.DrawImage(nowimg, this.Width / 2 - size / 2 + delta / 2, this.Height - size + delta, size - delta, size - delta);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (nowrecording == true)
+            {
+                recorder.StopRecording();
+                nowrecording = false;
+            }
+            recorder = new WaveIn();
+            recorder.DeviceNumber = comboBox1.SelectedIndex;
+            recorder.DataAvailable += waveIn_DataAvailable;
+            int sampleRate = 8000;
+            int channels = 1;
+            recorder.WaveFormat = new WaveFormat(sampleRate, channels);
+            recorder.StartRecording();
+            nowrecording = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (groupBox1.Visible)
+                groupBox1.Visible = false;
+            else
+                groupBox1.Visible = true;
+        }
+
+        private void Form1_AutoSizeChanged(object sender, EventArgs e)
+        {
+            this.ReDraw();
+        }
+    }
+}
